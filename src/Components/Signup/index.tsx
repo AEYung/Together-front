700f03bbfa36e2e156bb01e8f8228e894bb83812
abc <abc@example.com>
2022-12-21 09:52:48 +0900

    edit signupPage

--- src/Components/Signup/index.tsx
+++ src/Components/Signup/index.tsx
@@ -4,8 +4,42 @@ import * as S from './style';
 
 export default function Signup(){
     return(
-        <div>
-            <h1 css={S.Test}>테스트2</h1>
+        <div css={S.Positioner}>
+            <div css={S.LogoWrapper}>
+                <img src={require('../../Asset/logo.png')} alt='logo' />
+            </div>
+            <div css={S.EmailContainer}>
+                <input 
+                    type="text"
+                    placeholder="이메일"
+                    required
+                />
+                <button>인증번호 받기</button>
+            </div> 
+            <div css={S.EmailContainer}>
+                <input
+                    type="text"
+                    placeholder="인증번호 확인"
+                    required
+                />
+                <button>인증 확인</button>
+            </div>
+            <input css={S.InputStyle}
+                type="text"
+                placeholder="닉네임"
+                required
+            />
+            <input css={S.InputStyle}
+                type="password"
+                placeholder="비밀번호"
+                required
+            />
+            <input css={S.InputStyle}
+                type="password"
+                placeholder="비밀번호 확인"
+                required
+            />
+            <button css={S.ButtonStyle}>회원가입</button>
         </div>
     );
 }
--- src/Components/Signup/style.ts
+++ src/Components/Signup/style.ts
@@ -1,7 +1,78 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 
-export const Test = css`
-    font-size: 30px;
+export const Positioner = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(-60deg, #A0C9D2 50%, #FFFFFF 50%);;
+    height: 100vh;
+`
+
+export const LogoWrapper = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1vw;
+
+    img{
+        width:100%;
+    }
+`
+
+export const EmailContainer = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center; 
+    margin: 1vw;
+
+    input{
+        width: 15vw;
+        height: 6vh;
+        outline: none;
+		border: #41A1B6 2px solid;
+        border-radius: 7.5px;
+        box-sizing: border-box;
+        box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+        padding-left: 1vw;
+        margin-right: 0.5vw;
+    }
+
+    button{
+        width: 5.5vw;
+        height: 6vh;
+		outline: none;
+        border: none;
+        border-radius: 7.5px;
+		background-color: #41A1B6;
+        box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+		color: #FFFFFF;
+    }
+`
+
+export const InputStyle = css`
+    width: 21vw;
+    height: 6vh;
+    outline: none;
+    border: #41A1B6 2px solid;
+    border-radius: 7.5px;
+    box-sizing: border-box;
+    box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+    padding-left: 1vw;
+    margin: 1vw;
+`
+
+export const ButtonStyle = css`
+    width: 21vw;
+    height: 6vh;
+    outline: none;
+    border: none;
+    border-radius: 25px;
+    box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+    background-color: #41A1B6;
+    margin: 1.5vw;
+    color: white;
+    font-size: 19px;
     font-weight: bold;
 `
