d9729240d3aef241ea033821a70ab07d062a1659
abc <abc@example.com>
2022-12-22 09:40:31 +0900

    signin 페이지 제작

--- src/Components/Signin/index.tsx
+++ src/Components/Signin/index.tsx
@@ -1,12 +1,66 @@
 /** @jsxImportSource @emotion/react */
 import './style';
 import * as S from './style';
+import { Link } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
+import { emailState, passwordState } from '../../Atoms';
 
-export default function Signin(){
-    return(
-        <div>
 
+const TrySignin = () => {
+    const [email, setEmail] = useRecoilState(emailState);
+    const [password, setPassword] = useRecoilState(passwordState);
+
+    const onSignin = async () => {
+
+        if (email === '')
+            return alert('이메일이 입력되지 않았습니다.');
+        else if (password === '')
+            return alert('비밀번호가 입력되지 않았습니다.');
+
+        alert('로그인이 완료되었습니다!');
+        //서버 연결시 추가코드 필요
+    };
+
+    return {
+        setEmail,
+        setPassword,
+        onSignin
+    };
+}
+
+
+export default function Signin() {
+    const {
+        setEmail,
+        setPassword,
+        onSignin,
+    } = TrySignin();
+    return (
+        <div css={S.Backgrond}>
+            <div css={S.Positioner}>
+                <div css={S.LogoWrapper}>
+                    <img src={require('../../Asset/logo.png')} alt='logo' />
+                </div>
+                <input css={S.InputStyle}
+                    type="text"
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="이메일"
+                    required
+                />
+                <span css={S.AlertText}>존재하지 않는 이메일입니다.</span>
+                <input css={S.InputStyle}
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="비밀번호"
+                    required
+                />
+                <span css={S.AlertText}>비밀번호가 일치하지 않습니다.</span>
+                <button css={S.ButtonStyle} onClick={() => { onSignin(); }}>로그인</button>
+                <div css={S.GoToSignup}>
+                    <span>계정이 없으신가요?</span>
+                    <span><Link to="/signup">회원가입</Link></span>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/Signin/style.ts
+++ src/Components/Signin/style.ts
@@ -1,2 +1,76 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
+
+export const Backgrond = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(-60deg, #A0C9D2 50%, #FFFFFF 50%);
+`
+
+export const Positioner = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 21vw;
+    height: 100vh;
+`
+
+export const LogoWrapper = css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1vw;
+
+    img{
+        width:100%;
+    }
+`
+
+export const InputStyle = css`
+    width: 21vw;
+    height: 6vh;
+    outline: none;
+    border: #41A1B6 2px solid;
+    border-radius: 7.5px;
+    box-sizing: border-box;
+    box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+    padding-left: 1vw;
+    margin-top: 1vw;
+`
+
+export const AlertText = css`
+    opacity: 0;
+    color: #E21616;
+    font-size: 13px;
+    align-self: flex-start;
+    margin-top: 5px;
+`
+
+export const ButtonStyle = css`
+    width: 21vw;
+    height: 6vh;
+    outline: none;
+    border: none;
+    border-radius: 25px;
+    box-shadow: 2px 4px 5px rgba(0, 0, 0, 0.25);
+    background-color: #41A1B6;
+    margin: 3vw 0px 20px 0px;
+    color: white;
+    font-size: 19px;
+    font-weight: bold;
+`
+
+export const GoToSignup = css`
+    span{
+        color: white;
+        margin: 0px 5px 0px 5px;
+        a{
+            color: white;
+            text-decoration: underline;
+        }
+    }
+`
