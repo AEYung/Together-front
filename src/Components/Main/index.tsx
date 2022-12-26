ee40c651d8a8f4a6138577c34aa0e47609388041
JYG <jojeayun900@gmail.com>
2022-12-26 20:01:13 +0900

    add placeholder

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -9,7 +9,7 @@ export default function Main() {
         <div css={S.Positioner}>
             <div css={S.FlexContainer}>
                 <div css={S.SearchBox}>
-                    <input /><div><I.Search /></div>
+                    <input placeholder='참가하고 싶은 스터디룸 이름을 검색해 보세요!' /><div><I.Search /></div>
                 </div>
                 <button css={S.ButtonStyle}>스터디룸 생성</button>
                 <div css={S.BoxList}>
