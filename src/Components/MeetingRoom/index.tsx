73b758c8a3ec42326d3495da6611946d2006f8a8
hyein0112 <bhi12134@gmail.com>
2022-12-26 10:41:19 +0900

    UserBox

--- src/Components/MeetingRoom/index.tsx
+++ src/Components/MeetingRoom/index.tsx
@@ -1,3 +1,41 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { Link } from 'react-router-dom';
+import * as S from './style';
+import * as I from '../../Asset/svg';
 export default function MeetingRoom() {
-  return <h1>미팅룸</h1>;
+  return (
+    <div
+      css={css`
+        background: #f1f1f1;
+      `}
+    >
+      <UserBox />
+    </div>
+  );
+}
+
+function UserBox() {
+  return (
+    <S.UserContainer>
+      <S.UserContainerBox />
+      <S.Text css={{ marginTop: '13px' }}>
+        <Link css={{ color: '#E21616' }} to={'/'}>
+          회의 나가기
+        </Link>
+      </S.Text>
+      <S.ButtonBox>
+        <S.Button>
+          <I.OffMic />
+        </S.Button>
+        <S.Button>
+          <I.OffHeadset />
+        </S.Button>
+        <S.Button>
+          <I.OffVideo />
+        </S.Button>
+      </S.ButtonBox>
+      <S.Text css={{ alignSelf: 'flex-start' }}>참가인원: {'4'}</S.Text>
+    </S.UserContainer>
+  );
 }
---
+++ src/Components/MeetingRoom/style.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const UserContainer = styled.div`
+  width: 70.83vw;
+  height: 100vh;
+  margin-left: 1.67vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const UserContainerBox = styled.div`
+  width: 100%;
+  height: 82.04%;
+  background: #ffffff;
+  border: 2px solid #41a1b6;
+  border-radius: 20px;
+`;
+
+export const Text = styled.div`
+  font-size: 1.25rem;
+  font-weight: 600;
+  align-self: flex-end;
+`;
+
+export const ButtonBox = styled.div`
+  align-self: center;
+  display: flex;
+  justify-content: space-between;
+  width: 18.7vw;
+  margin-top: 9px;
+`;
+export const Button = styled.div`
+  cursor: pointer;
+`;
