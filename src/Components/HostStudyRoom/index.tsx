b2424e7937bd1e23d523315771f699c8809e362f
hyein0112 <bhi12134@gmail.com>
2022-12-27 20:08:47 +0900

    Setting Button

--- src/Components/HostStudyRoom/index.tsx
+++ src/Components/HostStudyRoom/index.tsx
@@ -1,8 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import MemberStudyRoom from '../MemberStudyRoom';
+import * as I from '../../Asset/svg';
+import * as S from './style';
+
 export default function HostStudyRoom() {
   return (
     <>
       <MemberStudyRoom />
+      <S.Container>
+        <div css={{ cursor: 'pointer' }}>
+          <I.Setting />
+        </div>
+      </S.Container>
     </>
   );
 }
---
+++ src/Components/HostStudyRoom/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  position: absolute;
+  top: 11.11vh;
+  right: 2.81vw;
+  display: flex;
+  flex-direction: column;
+`;
