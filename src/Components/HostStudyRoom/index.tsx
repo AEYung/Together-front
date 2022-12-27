15eea5af41fed9c04773a892e7f6424465243415
hyein0112 <bhi12134@gmail.com>
2022-12-27 20:35:37 +0900

    클립보드 기능

--- src/Components/HostStudyRoom/index.tsx
+++ src/Components/HostStudyRoom/index.tsx
@@ -19,6 +19,16 @@ export default function HostStudyRoom() {
 }
 
 function CopyInvitationCode() {
+  const copyCode: string = 'jdsfy89po4lsdjf09p3o';
+
+  const onClick = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('초대코드가 복사되었습니다!');
+    } catch (e) {
+      alert('초대코드 복사에 실패했습니다ㅜㅜ');
+    }
+  };
   return (
     <>
       <div css={{ display: 'flex', flexDirection: 'column' }}>
@@ -26,8 +36,12 @@ function CopyInvitationCode() {
         <S.Descript>클릭하여 복사하기</S.Descript>
 
         <S.InvitationCodeBox>
-          <span>jdsfy89po4lsdjf09p3o</span>
-          <S.CopyButton>
+          <span>{copyCode}</span>
+          <S.CopyButton
+            onClick={() => {
+              onClick(copyCode);
+            }}
+          >
             <I.Copy />
           </S.CopyButton>
         </S.InvitationCodeBox>
